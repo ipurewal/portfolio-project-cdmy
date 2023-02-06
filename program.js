c38448c6418f1396e423605d85cc48bb38da30e4
ipurewal <120794144+ipurewal@users.noreply.github.com>
2023-02-06 15:38:46 +0000

    commit 3

---
+++ program.js
@@ -0,0 +1,16 @@
+
+var credit_card = document.getElementById("project1")
+var project1img = document.getElementById("prj1img")
+prj1img.style.display = "none"
+function colourChange(){
+ credit_card.style.color = "red"
+}
+function showproject1(){
+if(prj1img.style.display === "none"){
+ prj1img.style.display ="block";}
+ else{
+prj1img.style.display = "none";}
+}
+credit_card.onmouseover = colourChange
+credit_card.onmouseleave = ()=>{credit_card.style.color = ""}
+credit_card.onclick = showproject1
